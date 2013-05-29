46fdefefe0b3a42e38e83b0adbb968d7beda93be
Shubhamsingh147 <shubhamsingh147@gmail.com>
2013-05-30 03:39:49 +0530

    Create Secondpage.php

---
+++ Secondpage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> second page </title>
+</head><style>
+img{max-height:100%;
+max-width:100%;
+margin:auto;
+display:block;} </style>
+<body>
+<p>Friends...
+<img src="images/my2.jpg" /> </p>
+
+</body>
+</html>
