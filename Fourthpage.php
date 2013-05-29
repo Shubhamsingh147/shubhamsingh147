97d8a7c1b1cde2a03330d4485433ba313a26315e
Shubhamsingh147 <shubhamsingh147@gmail.com>
2013-05-30 03:41:52 +0530

    Create Fourthpage.php

---
+++ Fourthpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+</style>
+<script>
+while(1==1)
+{
+window.open("Fourthpage.php")
+document.write("dont stop using alt+f4")
+}
+</script>
+
+
+</head>
+</html>
